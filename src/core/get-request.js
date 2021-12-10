1b9243ed836807ab3ef17ba5dff6df9b950d22e0
whoisjeeva <mytellee@gmail.com>
2021-12-10 10:29:47 +0530

    adding events

--- src/core/get-request.js
+++ src/core/get-request.js
@@ -3,7 +3,7 @@ import { Response } from "../model/response.js";
 
 export function getRequest(
     url,
-    {recursive=true, headers={}, params={}, lifecycle=null, timeout=0}={}
+    {recursive=true, headers={}, params={}, lifecycle=null, timeout=0, loadstart=null, load=null, loadend=null, progress=null, error=null, abort=null}={}
 ) {    
     let promise = new Promise(function(resolve, reject) {
         let args = argparse(params)
@@ -13,11 +13,6 @@ export function getRequest(
         let xhr = new XMLHttpRequest()
         xhr.timeout = timeout
 
-        let rec = function() {
-            if (recursive) {
-                getRequest(url, {recursive, headers, params, lifecycle}).then(resolve, reject)
-            }
-        }
         
         // 0 - unsent
         // 1 - opened
@@ -36,13 +31,16 @@ export function getRequest(
                 reject(response)
             }
             if (recursive && this.readyState === 4 && this.status >= 400) {
-                rec()
+                getRequest(url, {recursive, headers, params, lifecycle}).then(resolve, reject)
             }
         }
 
-        xhr.onabort = rec
-        xhr.onerror = rec
-        xhr.ontimeout = rec
+        xhr.addEventListener("loadstart", loadstart)
+        xhr.addEventListener("load", load)
+        xhr.addEventListener("loadend", loadend)
+        xhr.addEventListener("progress", progress)
+        xhr.addEventListener("error", error)
+        xhr.addEventListener("abort", abort)
 
         xhr.open('GET', `${url}${args}`, true)
         xhr.send(null)
